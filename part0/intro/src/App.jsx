b46483622bd7a92a7ef8ae9e35375bd0e324fbbf
han.le@aalto.fi <le.nm.han@gmail.com>
2024-03-03 23:26:13 +0200

    made components Display and Button to make more buttons

--- part0/intro/src/App.jsx
+++ part0/intro/src/App.jsx
@@ -1,18 +1,54 @@
 import { useState } from "react"
 
+const Display = (props) => {
+  return (
+    <div>{props.counter}</div>
+  )
+}
+
+const Button = (props) => {
+  return (
+    <button onClick={props.onClick}>
+      {props.text}
+    </button>
+  )
+}
+
 const App = () => {
   const [ counter, setCounter ] = useState(0)
 
+  const increaseByOne = () => {
+    console.log('increasing, value before', counter)
+    setCounter(counter + 1)
+  }
+
+  const decreaseByOne = () => { 
+    console.log('decreasing, value before', counter)
+    setCounter(counter - 1)
+  }
+
+  const setToZero = () => {
+    console.log('resetting to zero, value before', counter)
+    setCounter(0)
+  }
+
   return (
     <div>
-      <div>{counter}</div>
-      <button onClick={() => setCounter(counter + 1)}>
-        Plus 1
-      </button>
+      <Display counter={counter}/>
+      <Button 
+        onClick={increaseByOne}
+        text="plus 1"
+      />
 
-      <button onClick={() => setCounter(0)}> 
-        Reset
-      </button>
+      <Button
+        onClick={setToZero}
+        text='zero'
+      />     
+
+      <Button
+        onClick={decreaseByOne}
+        text='minus'
+      />           
     </div>
   )
 }
