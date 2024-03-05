42944b65701fcce08e771c4f0cdbe9a7d5adbf56
han.le@aalto.fi <le.nm.han@gmail.com>
2024-03-05 21:18:08 +0200

    Refactored and made component StatisticLine for displaying a single statistic

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -16,19 +16,35 @@ const Button = (props) => {
   )
 }
 
-const Statistics = (props) => {
-  return (
-    <p>
+// displays a single statistic
+const StatisticLine = (props) => {
+  if (props.value === props.value) {  // check if value is not NaN
+    return (
+      <p>
       {props.text} {props.value}
-    </p>
-  )
+      </p>
+    )
+  }
 }
 
-const Total = (props) => {
+const Statistics = (props) => {
+  const { good, neutral, bad } = props // Destructuring
+  if (good + neutral + bad == 0) {
+    return (
+      <p>
+        No feedback given.
+      </p>
+    )
+  }
   return (
-    <p>
-      {props.text} {props.count} {props.text2}
-    </p>
+    <div>
+      <StatisticLine text="good" value ={good} />
+      <StatisticLine text="neutral" value ={neutral} />
+      <StatisticLine text="bad" value ={bad} />
+      <StatisticLine text="total" value ={good + neutral + bad} />
+      <StatisticLine text="average" value ={(good - bad) / (good + neutral + bad)} />
+      <StatisticLine text="positive" value ={good / (good + neutral + bad) * 100  + "%"}/>
+    </div>
   )
 }
 
@@ -37,32 +53,16 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const [total, setTotal] = useState(0)
-  const [score, setScore] = useState(0)
-
-
   const handleGood = () => {
-    const updatedGood = good + 1
-    setGood(updatedGood)
-    setTotal(updatedGood + bad + neutral)
-    setScore(score + 1)
-    console.log('current score', score)
+    setGood(good + 1)
   }
 
   const handleNeutral = () => {
-    console.log('add a neutral rating')
-    const updatedNeutral = neutral + 1
-    setNeutral(updatedNeutral)
-    setTotal(updatedNeutral + bad + good)
-    setScore(score + 0)
+    setNeutral(neutral + 1)
   }
 
   const handleBad = () => {
-    console.log('add a bad rating')
-    const updatedBad = bad + 1
-    setBad(updatedBad)
-    setTotal(updatedBad + good + neutral)
-    setScore(score - 1)
+    setBad(bad + 1)
   }
   
   return (
@@ -80,13 +80,10 @@ const App = () => {
         onClick={handleBad} 
         text='bad'
       />
-      <Header text='Statistics'/>
-      <Statistics text='good: ' value={good}/>
-      <Statistics text='neutral: ' value={neutral}/>
-      <Statistics text='bad: ' value={bad}/>
-      <Statistics text='total: ' value={total}/>
-      <Statistics text='average: ' value={score/total}/>
-      <Statistics text='positive percentage: ' value={good/total * 100}/>
+      <Header text="Statistics" />
+      <Statistics
+        good={good} neutral={neutral} bad={bad}
+      />
     </div>
   )
 }
