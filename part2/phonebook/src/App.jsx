c901ba3a2cb1fd27b44ea0411af83a72c914ff63
han.le@aalto.fi <le.nm.han@gmail.com>
2024-03-09 23:39:00 +0200

    add event handlers and sates for inputting name

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,27 +1,58 @@
 import { useState } from 'react'
+import Person from './components/Person'
 import './index.css'
 
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas' }
   ]) 
+  const [newName, setNewName] = useState('') // controlled component for storing the user-submitted input
+  
+  // event handler, parameter event triggers the function addName
+  const addName = (event) => {
+    event.preventDefault()  // prevents the default action of submitting a form, e.g., reloading page
+    console.log('button clicked', event.target)
 
-  // newName state is for controlling the form input element
-  const [newName, setNewName] = useState('')
+    const personObject = {
+      name: newName,
+    }
+
+    setPersons(persons.concat(personObject)) // creates a new copy of the array with the new item concatenated
+    setNewName('') // resets the value
+  }
+
+  // event handler for input change
+  const handleChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addName}>
         <div>
-          name: <input />
+          name: <input 
+                  value={newName}
+                  onChange={handleChange}
+                />
         </div>
+        <br></br>
         <div>
-          <button type="submit">add</button>
+          <button onClick={addName} type="submit">add</button>
         </div>
       </form>
+
       <h2>Numbers</h2>
-      ...
+      <div>
+        {console.log("current list", persons)}
+        <ul>
+          {persons.map(p => 
+            <Person person={p} key={p.name}/>
+          )}
+        </ul>
+      </div>
+      
     </div>
   )
 }
