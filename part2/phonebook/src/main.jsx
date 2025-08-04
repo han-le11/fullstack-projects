cac374afb9c822e39c2fe91383f45b2f79f5fc63
han.le@aalto.fi <le.nm.han@gmail.com>
2025-08-04 11:35:25 +0300

    init commit

--- part2/phonebook/src/main.jsx
+++ part2/phonebook/src/main.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './App'
 import './index.css'
+
+// axios.get('http://localhost:3001/persons').then(response => {
+//   const persons = response.data
+//   ReactDOM.createRoot(document.getElementById('root')).render(<App  />)
+// })
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
